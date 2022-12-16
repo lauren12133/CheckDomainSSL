df1fc0c04c26a39ec9e8dd5361c1f1b3502beb82
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2022-12-16 16:40:03 +0800

    Update cdssl.sh

--- cdssl.sh
+++ cdssl.sh
@@ -1,76 +1,64 @@
 #!/bin/bash
+set -x 
+# 设置域名 端口默认443
+domains=( abc.com cbd,com )
+port=443
 
-port=***
+# 低于多少天通知快过期域名默认5天
+threshold=5
 
-#tg_bot通知
+# 多久运行一次单位秒默认3天
 
-#bot_token
-TOKEN=***
-#bot_id
-CHAT_ID=***
-#下面不懂不要动
-URL="https://thingproxy.freeboard.io/fetch/https://api.telegram.org/bot$TOKEN/sendMessage"
+sleep_run(){
+        sleep 259200
+}
 
+# TGbot
+api_key="****"
+chat_id="*****"
+#API地址不懂不要动
+URL=https://thingproxy.freeboard.io/fetch/https://api.telegram.org/bot$api_key/sendMessage
 
-#检测https证书有效
-echo '开始检查 https证书有效期'
+# opensslpath
+openssl_path="/usr/bin/openssl"
 
-#定义错误的域名
-errorDominStr=""
-
-while read line; do
-    echo "====================================================================================="
-    echo "当前检测的域名：" $line
-    end_time=$(echo | timeout 1 openssl s_client -servername $line -connect $line:$port 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null | awk -F '=' '{print $2}' )
-    ([ $? -ne 0 ] || [[ $end_time == '' ]]) && echo '该域名链接不上,跳到下一个域名' && continue
-    
-    end_times=`date -d "$end_time " +%s`
-    current_times=`date -d "$(date -u '+%b %d %T %Y GMT') " +%s`
-
-	echo "截至日期: " $end_times
-	echo "当前日期: " $current_times
-	
-    
-    let left_time=$end_times-$current_times
-    days=`expr $left_time / 86400`
-    echo "剩余天数: " $days
-    
-    [ $days -lt 5 ] && echo "https 证书有效期少于5天，存在风险"  && errorDominStr="$errorDominStr  $line"
-    
-done < $(pwd)/checkdominssl/https_list.txt
+# temp_file
+temp_file="/tmp/ssl_cert_info.txt"
 
 
-echo -e "准备过期的域名为： \n  $errorDominStr"
 
-if [ "$errorDominStr" = "" ]
-then
-  echo "不包含准备过期的域名"
-  bgq="👍︎👍︎👍︎不存在低于5天准备过期的域名
+while true; do
+  # 逻辑判断域名缓存
+  expired_domains=""
+  remaining_domains=""
 
-👋👋👋︎Good bye!"
+  for domain in "${domains[@]}"; do
+    $openssl_path s_client -servername $domain -connect $domain:$port < /dev/null 2> /dev/null | $openssl_path x509 -noout -enddate > $temp_file
 
-  MESSAGE=$bgq
-  
-  curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE" 
-else
-  echo "包含准备过期的域名"
-  
-  gq="💀💀💀存在不到5天就过期的域名
-  
-包含准备过期的域名:
+    expiration_date=$(grep "notAfter" $temp_file | cut -d'=' -f2)
 
-$errorDominStr
+    expiration_timestamp=$(date -d "$expiration_date" +%s)
 
-👆︎👆︎👆︎快去续约上方域名吧
-  "
-  
-  MESSAGE=$gq
-  
-  curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$MESSAGE"
-  
-  exit 10
-fi
+    current_timestamp=$(date +%s)
 
-echo "Good bye!"
+    days_remaining=$(( (expiration_timestamp - current_timestamp) / 86400 ))
 
-exit 0
+    if [ $days_remaining -lt $threshold ]; then
+      expired_domains="$expired_domains $domain"
+    else
+      remaining_domains="$remaining_domains $domain ($days_remaining 剩余有效时长)"
+    fi
+  done
+
+  if [ ! -z "$expired_domains" ]; then
+    message="💀💀💀存在5天快过期的域名:$expired_domains"
+    curl -s -X POST $URL -d chat_id=$chat_id -d text="$message" > /dev/null
+  fi
+
+  if [ ! -z "$remaining_domains" ]; then
+    message="👍︎👍︎👍︎没有SSL快过期的域名$remaining_domains"
+    curl -s -X POST $URL -d chat_id=$chat_id -d text="$message" > /dev/null
+  fi
+sleep_run
+done
+
