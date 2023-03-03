0f4cb9471628f2f06873782cc350951864648de7
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2023-03-03 12:04:14 +0800

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -13,7 +13,7 @@ URL=https://thingproxy.freeboard.io/fetch/https://api.telegram.org/bot$api_key/s
 
 while true; do 
   temp=$[$(cat /sys/class/thermal/thermal_zone0/temp)/1000]
-  if [ "$temp" -gt 30 ]; then
+  if [ "$temp" -gt 60 ]; then
     curl -X POST $URL -d chat_id=$chat_id -d text="💀💀💀Pine64当前温度过高: $temp°💀💀💀" > /dev/null
   else
     curl -X POST $URL -d chat_id=$chat_id -d text="👍︎👍︎👍︎text":"当前温度: $temp°👍︎👍︎👍︎" > /dev/null
