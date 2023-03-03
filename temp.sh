673380063dfb3e4764ff2eb79a300aa956bc8264
lauren12133 <77604108+lauren12133@users.noreply.github.com>
2023-03-03 11:04:34 +0800

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -4,17 +4,16 @@ set -x
 sleep_run(){
         sleep 10800
 }
-
 # TGbot
-api_key="****"
+api_key="***"
 chat_id="*****"
 #API地址不懂不要动
 URL=https://thingproxy.freeboard.io/fetch/https://api.telegram.org/bot$api_key/sendMessage
 
 
 while true; do 
-  temp=$(cat /sys/class/thermal/thermal_zone0/temp)/1000 
-  if [ "$temp" -gt 60.0 ]; then
+  temp=$[$(cat /sys/class/thermal/thermal_zone0/temp)/1000]
+  if [ "$temp" -gt 30 ]; then
     curl -X POST $URL -d chat_id=$chat_id -d text="💀💀💀Pine64当前温度过高: $temp°💀💀💀" > /dev/null
   else
     curl -X POST $URL -d chat_id=$chat_id -d text="👍︎👍︎👍︎text":"当前温度: $temp°👍︎👍︎👍︎" > /dev/null
